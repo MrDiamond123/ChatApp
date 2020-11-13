97fe68af1e9a601cdbd1e9c68205b624e5c4a23f
MrDiamond123 <kurtisiscool123@gmail.com>
2020-11-13 08:18:58 -0500

    Added comments to possibly understand the code better, and notes to myself to add things that I will ignore

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const session = require('express-session')
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
+//TODO: Actually add sessions
 const sessionMiddleware = session({ secret: 'cactus potato', cookie: { maxAge: 60000}})
 
 app.use(sessionMiddleware)
@@ -24,25 +25,35 @@ app.get('/', function(req, res) {
     res.render('index.ejs');
 });
 
+
+// Allow the client to access certain files
 app.use('/scripts', express.static(__dirname + '/node_modules/dompurify/dist/'));
 app.use('/media', express.static(__dirname + '/media'));
 
+
 io.sockets.on('connection', function(socket) {
+    // When a client joins
     socket.on('join', function(username) {
         socket.username = username;
         io.emit('user_connected', socket.username);
         console.log('🔵 ' + socket.username + ' joined the chat ')
     });
 
+    //When a client leaves
     socket.on('disconnect', function(username) {
         io.emit('user_disconnected', socket.username);
         console.log('🔴 ' + socket.username + ' left the chat')
     })
+
+    //When a client changes their nickname
     socket.on('nickname', function(username) {
         io.emit('user_changed_nickname', socket.username, username);
         console.log(socket.username + ' has changed their name to ' + username)
         socket.username = username;
     })
+
+    //When a client runs a command
+    //TODO: Make an actually good system for handling commands so I don't have to hardcode the help menu
     socket.on('command', function(command) {
         switch(command) {
             case "help":
@@ -61,6 +72,8 @@ io.sockets.on('connection', function(socket) {
                 socket.emit('command_feedback', command + " isn't a command!");
         }
     })
+
+    // When a client sends a message 
     socket.on('chat_message', function(message) {
         io.emit('chat_message', socket.username, message);
         console.log(socket.username + ' : ' + message)
